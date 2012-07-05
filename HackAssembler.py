695549484a3fca3ce98899852582013957ac4c0a
Richard Turner <rwturner17@gmail.com>
2012-07-05 01:14:33 -0400

    Working No-Symbol Assembler
    This assembler appears to function when the .asm file contains no symbols.
    Next version will work with symbols

--- HackAssembler.py
+++ HackAssembler.py
@@ -1,1 +1,47 @@
 ## Hack Assembler
+
+import hackparser
+import hackcodegen
+
+def assembler(filename):
+    asmfilename = filename.split('\\')[-1]
+    programname = asmfilename.split('.')[0]
+    folderlist = filename.split('\\')[0:-1]
+    folderlist = [s + '\\' for s in folderlist]
+    folder = ''.join(folderlist)
+    hackfilename = folder + programname + '.hack'
+    
+    asmfile = open(filename, 'r')
+    hackfile = open(hackfilename, 'w')
+
+    for line in asmfile:
+        print line
+        line = line.split('//', 1)[0]
+        line = line.strip()
+        if line == '':
+            continue
+        print line
+        if hackparser.commandType(line) == 'A_COMMAND':
+            value = dec2val(hackparser.symbol(line))
+            hackfile.write('0' + value + '\n')
+            print '0' + value
+        elif hackparser.commandType(line) == 'C_COMMAND':
+            bincmd = ('111' + hackcodegen.comp[hackparser.comp(line)] +
+                      hackcodegen.dest[hackparser.dest(line)] +
+                      hackcodegen.jmp[hackparser.jmp(line)])
+            hackfile.write(bincmd + '\n')
+            print bincmd
+
+    asmfile.close()
+    hackfile.close()
+            
+def dec2val(numstr):
+    num = int(numstr)
+    binnum = bin(num)
+    binnum = binnum.split('b')[-1]
+    binnum = binnum.zfill(15)
+    binnum = binnum[-15:]
+    return binnum
+            
+        
+    
--- hackparser.py
+++ hackparser.py
@@ -1,2 +1,43 @@
 ## Hack Assembly Parser
 
+allowed_non_alpha_char = ['_', '.', ':', '$', '0', '1', '-', '!']
+
+def commandType(command):
+    if command == '':
+        return None
+    if command[0] == '@':
+        return 'A_COMMAND'
+    elif command[0] == '(':
+        return 'L_COMMAND'
+    elif (command[0].isalpha() or command[0] in allowed_non_alpha_char):
+        return 'C_COMMAND'
+    else:
+        return None
+
+def symbol(command):
+    if command[0] == '@':
+        return command[1:]
+    elif command[0] == '(':
+        return command[1:-1]
+    else:
+        return None
+
+def dest(command):
+    if '=' in command:
+        return command.split('=')[0]
+    else:
+        return 'null'
+
+def comp(command):
+    assign = (command.split(';')[0])
+    return assign.split('=')[-1]
+
+def jmp(command):
+    if ';' in command:
+        return (command.split(';')[-1])
+    else:
+        return 'null'
+    
+
+
+
