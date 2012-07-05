1f1a1f7c0c6d8c1d4d062ae08d40b006ed43950c
Richard Turner <rwturner17@gmail.com>
2012-07-05 01:36:03 -0400

    Working Symbolic Assembler
    Assembler appears to work with symbols.
    
    Next version should include symbol assignment macros.

--- HackAssembler.py
+++ HackAssembler.py
@@ -2,6 +2,7 @@
 
 import hackparser
 import hackcodegen
+import hacksymtab
 
 def assembler(filename):
     asmfilename = filename.split('\\')[-1]
@@ -14,17 +15,47 @@ def assembler(filename):
     asmfile = open(filename, 'r')
     hackfile = open(hackfilename, 'w')
 
+    symtab = hacksymtab.HackSymbolTable()
+
+    ## First pass
+    ROMaddr = 0
+    for line in asmfile:
+        line = (line.split('//', 1)[0])
+        line = line.strip()
+        if line == '':
+            continue
+        commandType = hackparser.commandType(line)
+        if (commandType == 'A_COMMAND' or commandType == 'C_COMMAND'):
+            ROMaddr += 1
+        elif commandType == 'L_COMMAND':
+            assert not symtab.contains(hackparser.symbol(line))
+            symtab.addEntry(hackparser.symbol(line), ROMaddr + 1)
+
+    asmfile.close()
+    asmfile = open(filename, 'r')
+        
+    ## Second pass
+    nextfreeRAM = 16
     for line in asmfile:
         print line
-        line = line.split('//', 1)[0]
+        line = (line.split('//', 1)[0])
         line = line.strip()
         if line == '':
             continue
         print line
         if hackparser.commandType(line) == 'A_COMMAND':
-            value = dec2val(hackparser.symbol(line))
-            hackfile.write('0' + value + '\n')
-            print '0' + value
+            if line[1].isdigit():
+                value = str(line[1:])
+            else:
+                symbol = hackparser.symbol(line)
+                if symtab.contains(symbol):
+                    value = str(symtab.getAddress(symbol))
+                else:
+                    symtab.addEntry(symbol, nextfreeRAM)
+                    value = str(nextfreeRAM)
+                    nextfreeRAM += 1
+            hackfile.write('0' + dec2val(value) + '\n')
+            print '0' + dec2val(value)
         elif hackparser.commandType(line) == 'C_COMMAND':
             bincmd = ('111' + hackcodegen.comp[hackparser.comp(line)] +
                       hackcodegen.dest[hackparser.dest(line)] +
